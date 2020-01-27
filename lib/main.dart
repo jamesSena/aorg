af1b4f1b514027dbacd4f93f8d4f1fe0f737c6a7
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-27 14:55:50 -0300

    criando uma container (label) para exibir o resultado

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,28 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 50,),
+                Text(
+                  "Compensa utilizar álcool",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color:Theme.of(context).primaryColor,
+                    fontSize: 40,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  
+                ),
+              ],
+            ),
+          ),
           Input(
             label:"Gasolina",
             ctrl: _gasCtrl,
@@ -47,7 +69,17 @@ class HomePage extends StatelessWidget {
 
             ),
             child: FlatButton(
-              child: Text("CALCULAR"),
+              child: Text(
+                "CALCULAR",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 25,
+                  fontFamily: "Big Shoulders Display",
+                  ),
+                
+                
+                ),
+              
               onPressed: (){},
             ),
           ),
