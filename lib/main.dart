eb1209128e8c453fea6a8a0dda9bacc9fc0cf2f8
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-27 15:19:47 -0300

    Separando o botão do Main. Um unico botão com duas responsabilidade.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:aorg/widget/input.widget.dart';
+import 'package:aorg/widget/loading-button.widget.dart';
 import 'package:aorg/widget/logo.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
@@ -49,31 +50,13 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 20,),
-                 Container(
-                  margin: EdgeInsets.all(30),
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(60),
-
-                  ),
-                  child: FlatButton(
-                    child: Text(
-                      "CALCULAR NOVAMENTE",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 25,
-                        fontFamily: "Big Shoulders Display",
-                        ),
-                      
-                      
-                      ),
-                    
-                    onPressed: (){},
-                  ),
-                ),
-              ],
+              LoadingButton(
+            busy: false,
+            func: (){},
+            text: "CALCULAR NOVAMENTE",
+            invert: true,
+          ),
+           ],
 
               
             ),
@@ -86,31 +69,14 @@ class HomePage extends StatelessWidget {
             label:"Álcool",
             ctrl: _alcCtrl,
           ),
-          Container(
-            margin: EdgeInsets.all(30),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(60),
-
-            ),
-            child: FlatButton(
-              child: Text(
-                "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: "Big Shoulders Display",
-                  ),
-                
-                
-                ),
-              
-              onPressed: (){},
-            ),
+          LoadingButton(
+            busy: true,
+            func: (){},
+            text: "CALCULAR",
+            invert: false,
           ),
         ],
-        
+              
       ),
     );
   }
