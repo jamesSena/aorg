59b72a9b0c8df11dad6597e6b539439b298e727e
Jamesson Sales de Sena <jsdesena@interaxa.com>
2020-01-27 15:03:56 -0300

    Criando um botão para calcular novamente

--- lib/main.dart
+++ lib/main.dart
@@ -47,9 +47,35 @@ class HomePage extends StatelessWidget {
                     fontSize: 40,
                     fontFamily: "Big Shoulders Display",
                   ),
-                  
+                ),
+                SizedBox(height: 20,),
+                 Container(
+                  margin: EdgeInsets.all(30),
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(60),
+
+                  ),
+                  child: FlatButton(
+                    child: Text(
+                      "CALCULAR NOVAMENTE",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 25,
+                        fontFamily: "Big Shoulders Display",
+                        ),
+                      
+                      
+                      ),
+                    
+                    onPressed: (){},
+                  ),
                 ),
               ],
+
+              
             ),
           ),
           Input(
